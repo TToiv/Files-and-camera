58935b172f8294110515d702664b0541a156e0a7
TToiv <toit1_15@uni.worc.ac.uk>
2018-12-11 15:06:15 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,11 +26,12 @@ function onDeviceReady() {
 	
     
     //following allows you to gain access to the supported platform specific locations that are shared by all applications (useful for stioring images, music etc. )
-    window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, success, fail);
+    window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, gotFS, fail);
 }
-
+alert("hey")
 //get access to file and CREATE if does not exists
 function gotFS(fileSystem) {
+
  	fileSystem.getFile("test.txt", {create: true, exclusive: false}, gotFileEntry, fail);
 }
 
